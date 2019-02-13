ca65bec48d80f9005cf8cbcd131cb10b17499ac3
Sally Estevez <sallyestevez318@gmail.com>
2019-02-13 01:04:17 +0000

    final touches

--- js/script.js
+++ js/script.js
@@ -40,6 +40,27 @@ function placement(){
         imgSrc="Chika.png";
     }
 }
+function description(){
+    if(memberPlacement === "Kanan"){
+        return $(".result").append("<h4>You're Kanan! You never lose your cool, even in tough situations. You're very supportive and put your friends' needs above your own.</4>");
+    }else if(memberPlacement === "Dia"){
+        return $(".result").append("<h4>You're Dia! You're very refined, polite, and a bit of a perfectionist.</h4>");
+    }else if(memberPlacement === "Hanamaru"){
+        return $(".result").append("<h4>You're Hanamaru! You're hardworking, an avid reader, and a bit out of the know when it comes to technology.</h4>");
+    }else if(memberPlacement === "Mari"){
+        return $(".result").append("<h4>You're Mari! You're very cheerful and you love making jokes!</h4>");
+    }else if(memberPlacement === "Yoshiko"){
+        return $(".result").append("<h4>You're Yoshiko! You're bright, self-assured, smart, and thoughtful!</h4>");
+    }else if(memberPlacement === "Riko"){
+        return $(".result").append("<h4>You're Riko! You're reserved and modest, but you can also crack under pressure.</h4>");
+    }else if(memberPlacement === "Ruby"){
+        return $(".result").append("<h4>You're Ruby! You're very shy and timid, but your friends support you no matter what.</h4>");
+    }else if(memberPlacement === "You"){
+        return $(".result").append("<h4>You're You! You're energetic, positive, and charismatic!</h4>");
+    }else if(memberPlacement === "Chika"){
+        return $(".result").append("<h4>You're Chika! You're very sociable and determined, but you also have a sensitive side.</h4>");
+    }
+}
 
     $("button").click(function() {
         memberPlacement = "TBD";
@@ -65,10 +86,5 @@ function placement(){
         totalScore = q1Result + q2Result + q3Result + q4Result + q5Result + q6Result + q7Result + q8Result + q9Result;
         placement();
         displayResult();
+        description();
     });
-    
-//Kanan description: You're Kanan! You never lose your cool, even in tough situations. You're very supportive and put your friends' needs above your own. 
-//Dia description: You're Dia! You're very refined, polite, and a bit of a perfectionist.
-//Hanamaru description: You're Hanamaru!
-//Mari description:
-//Yoshiko description:
